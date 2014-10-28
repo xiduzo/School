70bfd3d200ab10451a3375b38d1802f4208306f3
sander boer <boera006@hva.nl>
2014-10-28 15:57:36 +0100

    awesome nav hide on - nav ul li a - click
    handy for mobile

--- FED2/webappFunctioneel/static/js/app/functions.js
+++ FED2/webappFunctioneel/static/js/app/functions.js
@@ -2,4 +2,8 @@
 	$("#navHandleBar").click(function () {
         $('#navHandleBar').parent().toggleClass('activeNav');
     });
+
+    $("nav ul li a").click(function () {
+        $('#navHandleBar').parent().toggleClass('activeNav');
+    });
 })();
