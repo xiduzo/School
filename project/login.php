3ea8b0f3df4d5272f8f5852d344733d8e85d2671
Xiduzo <boera006@hva.nl>
2015-01-22 23:54:16 +0100

    deleting > making
    deleted all the usseless crap

--- project/login.php
+++ project/login.php
@@ -1,18 +1,16 @@
 <?
-session_start();
 
 include 'includes/addFunctions.php';
+include 'includes/getFunctions.php';
 include 'includes/templates.php';
 include 'includes/checks.php';
-
-checkUser();
 
 ?>
 <!doctype html>
 <html>
     <head>
     	<?
-    		head('login');
+    		head('Getting Started');
 
 	        addCSS('static/css/main.css');
 	        //font awesome
@@ -20,37 +18,138 @@ checkUser();
         ?>
     </head>
     <body>
+    	<?
+    		pageHeader();
+    	?>
 
 		<main id="mainContent">
-		<div id="background"></div>
-
-		<?
-			header("Location: http://sanderboer.nl/school/project/starting.php");
-			die();
-		?>
-
-		<? if($_POST['loginUser']): ?>
+			<div id="background"></div>
 			<?
-				$klantNummer = $_POST['klantNummer'];
-				$wachtwoord = $_POST['wachtwoord'];
-				checkLogin($klantNummer, $wachtwoord);
+				$point = 0;
+				if(isset($_POST['new'])) {
+					$point = 1;
+				} else if(isset($_POST['next1'])) {
+					$point = 2;
+				} else if(isset($_POST['next2'])) {
+					$point = 3;
+				} else if(isset($_POST['login']) || isset($_POST['start'])) {
+					$point = 4;
+				} else if($_POST['loginUser']) {
+					$point = 4;
+					$klantNummer 	= $_POST['klantNummer'];
+					$wachtwoord = $_POST['wachtwoord'];
+					checkLogin($klantNummer, $wachtwoord);
+				}
 			?>
-		<? else: ?>
-			<form id="loginForm" method="post" action="<?=$_SERVER['PHP_SELF']?>">
-				<legend>Inloggen Mijn WoonEnergie</legend>
 
-				<input type="text" name="klantNummer" placeholder="Klantnummer">
-				<input type="password" name="wachtwoord" placeholder="Wachtwoord">
+			<? if($point == 0): ?>
+				<section id="starting" class="stap1">
+					Met <span>WoonEnergie</span> <br/>
+					gemakkelijk besparen aan de hand van een persoonlijk doel
+					<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+						<button type="submit" name="new">Nu starten met besparen</button>
+						<button type="submit" name="login">Al een account? Log in</button>
+					</form>
+				</section>
+			<? elseif($point == 1): ?>
+				<section id="starting" class="stap2">
+					<article>
+						<h1>Woning gegevens</h1>
+						Om een beter, haalbaar en realistisch bespaardoel in te kunnen stellen hebben wij een aantal gegevens nodig
+					</article>
 
-				<input id="loginButton" type="submit" value="Inloggen" name="loginUser">
+					<article>
+						<h1>Type huis:</h1>
+						<ul id="house">
+							<li class="active">Appartement</li>
+							<li>Tussenwoning</li>
+							<li>Hoekwoning</li>
+							<li>Vrijstaand</li>
+						</ul>
+					</article>
 
-				<a href="/school/project/forgot.php">Inloggegevens vergeten?</a> <br/>
-				<a href="#aanvragen">Mijn WoonEnergie aanvragen?</a>
-			</form>
-		<? endif; ?>
+					<article>
+						<h1>Aantal personen in uw huishouden:</h1>
+						<ul id="persons">
+							<li class="active">1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5+</li>
+						</ul>
+					</article>
+					<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+						<button class="steps" type="submit" name="next1">Volgende 1/3</button>
+					</form>
+				</section>
+			<? elseif($point == 2): ?>
+				<section id="starting" class="stap3">
+					<article>
+						<h1>Bespaardoel instellen</h1>
+						Om effecient te besparen in het bewezen dat u sneller bespaart als u een bespaardoel instelt. 
+						Door deze in te vullen kunnen wij u sneller aan uw doel helpen door relevante tips en hulp aan te boeden
+					</article>
+
+					<article>
+						<h1>Kies hoeveel u wilt besparen:</h1>
+						<ul id="house">
+							<li class="active">&euro;60</li>
+							<li>&euro;90</li>
+							<li>&euro;250</li>
+							<li>Ander bedrag</li>
+						</ul>
+					</article>
+
+					<article>
+						<h1>Over hoeveel tijd:</h1>
+						<ul id="persons">
+							<li class="active">6 maanden</li>
+							<li>1 jaar</li>
+							<li>1.5 jaar</li>
+							<li>Ander termijn</li>
+						</ul>
+					</article>
+					<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+						<button class="steps" type="submit" name="next2">Volgende 2/3</button>
+					</form>
+				</section>
+			<? elseif($point == 3): ?>
+				<section id="starting" class="stap4">
+					<article>
+						<h1>Meterstand invoeren</h1>
+						Om cijfers te laten kloppen hebben we uw meterstanden nodig. 
+						Dankzij de slimme meter zullen deze gegevens automatisch bijgewerkt worden.
+					</article>
+
+					<article>
+						<i class="fa fa-plug"></i> <em>Electriciteit</em>
+						<input type="number" placeholder="Vul hier uw meterstand in" />
+						<i class="fa fa-tint"></i> <em>Water</em>
+						<input type="number" placeholder="Vul hier uw meterstand in" />
+						<i class="fa fa-fire"></i> <em>Gas</em>
+						<input type="number" placeholder="Vul hier uw meterstand in" />
+					</article>
+					<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+						<button class="steps" type="submit" name="start">Start met besparen</button>
+					</form>
+				</section>
+			<? elseif($point == 4): ?>
+					<form id="loginForm" method="post" action="<?=$_SERVER['PHP_SELF']?>">
+						<legend>Inloggen Mijn WoonEnergie</legend>
+
+						<input type="text" name="klantNummer" placeholder="Klantnummer">
+						<input type="password" name="wachtwoord" placeholder="Wachtwoord">
+
+						<input id="loginButton" type="submit" value="Inloggen" name="loginUser">
+						
+						<!-- 
+						<a href="#vergeten">Inloggegevens vergeten?</a> <br/>
+						<a href="#aanvragen">Mijn WoonEnergie aanvragen?</a> 
+						-->
+					</form>
+			<? endif; ?>				
 
 		</main>
-
 
 		<!-- JS scripts -->
 		<?
@@ -60,5 +159,17 @@ checkUser();
 
 			// Self
 		?>
+		<script>
+			// We all <3 quick fixes
+			$('#navButton').hide();
+
+			$('#starting ul li').click(function() {
+				$(this).siblings('.active').removeClass('active');
+				$(this).addClass('active');
+			});
+		</script>
+
+
+
     </body>
 </html>
